2f895b4fb395334e7864bf0d0726c906e04e2709
igmanoski <igor4003@gmail.com>
2026-07-15 19:57:53 +0200

    Fix missing placeholder replacements in email templates causing unreplaced colors

--- aftermarket-email-branding/templates/emails/email-footer.php
+++ aftermarket-email-branding/templates/emails/email-footer.php
@@ -8,6 +8,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Pobieramy wartości z bazy
+$bg_color    = get_option('woocommerce_email_background_color', '#0B0B14');
+$body_color  = get_option('woocommerce_email_body_background_color', '#121221');
 $footer_copy = get_option('am_email_footer_copy', '&copy; ' . date('Y') . ' Aftermarket.ag. Wszelkie prawa zastrzeżone.');
 
 // Pobieramy szablon z wtyczki
@@ -16,5 +18,8 @@ $footer_template = get_option('am_email_custom_footer_html', am_email_get_defaul
 // Podmieniamy znaczniki
 $footer_template = str_replace('{footer_copy}', wp_kses_post($footer_copy), $footer_template);
 $footer_template = str_replace('{site_title}', get_bloginfo('name', 'display'), $footer_template);
+$footer_template = str_replace('{bg_color}', esc_attr($bg_color), $footer_template);
+$footer_template = str_replace('{body_color}', esc_attr($body_color), $footer_template);
+$footer_template = str_replace('{border_color}', 'rgba(255,255,255,0.08)', $footer_template);
 
 echo $footer_template;
--- aftermarket-email-branding/templates/emails/email-header.php
+++ aftermarket-email-branding/templates/emails/email-header.php
@@ -9,6 +9,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Pobieramy wartości z bazy
 $base_color = get_option('woocommerce_email_base_color', '#F43F5E');
+$bg_color   = get_option('woocommerce_email_background_color', '#0B0B14');
+$body_color = get_option('woocommerce_email_body_background_color', '#121221');
+$text_color = get_option('woocommerce_email_text_color', '#D4D4D8');
 $logo_text  = get_option('am_email_logo_text', 'Aftermarket');
 
 // Pobieramy kod szablonu z ustawień wtyczki
@@ -19,6 +22,10 @@ $header_template = str_replace('{language_attributes}', is_rtl() ? 'rightmargin=
 $header_template = str_replace('{site_title}', get_bloginfo('name', 'display'), $header_template);
 $header_template = str_replace('{logo_text}', esc_html($logo_text), $header_template);
 $header_template = str_replace('{base_color}', esc_attr($base_color), $header_template);
+$header_template = str_replace('{bg_color}', esc_attr($bg_color), $header_template);
+$header_template = str_replace('{body_color}', esc_attr($body_color), $header_template);
+$header_template = str_replace('{text_color}', esc_attr($text_color), $header_template);
+$header_template = str_replace('{border_color}', 'rgba(255,255,255,0.08)', $header_template);
 $header_template = str_replace('{email_heading}', esc_html($email_heading), $header_template);
 
 echo $header_template;
